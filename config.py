b01f0973a7839b3db060c386d69a6789aff1a9d9
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-05-09 13:41:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,29 +12,29 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Specify where to get the following credentials
-OWNER_USERNAME = getenv("OWNER_USERNAME","PRADHAN474")
-BOT_USERNAME = getenv("BOT_USERNAME", "oxgenxBot")
-BOT_NAME = getenv("BOT_NAME", "𝐎𝐗𝐘𝐆𝐄𝐍 𝐗 𝐁𝐎𝐓")
-ASSUSERNAME = getenv("ASSUSERNAME", "Blossom_xmusic_bot_assitant")
-EVALOP = list(map(int, getenv("EVALOP", "5059737154").split()))
+OWNER_USERNAME = getenv("OWNER_USERNAME","llxHKxll")
+BOT_USERNAME = getenv("BOT_USERNAME", "PixieMusicBot")
+BOT_NAME = getenv("BOT_NAME", "Pixie Music")
+ASSUSERNAME = getenv("ASSUSERNAME", "PeachyTunesAssistant")
+EVALOP = list(map(int, getenv("EVALOP", "6329058409").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOGGER_ID = int(getenv("LOGGER_ID", -1001437104456))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002003303440))
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
 PLAYHT_API = getenv("PLAYHT_API")
-OWNER_ID = int(getenv("OWNER_ID", 5059737154))
+OWNER_ID = int(getenv("OWNER_ID", 6329058409))
 
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/PRADHAN474/OXYGEN")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Git-Hub-xD/PixieMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support and contact information - Provide your own support channels
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/OXEGN")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BWANDARLOK")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/XovaAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/KaisenWorld")
 
 # Server limits and configurations - These can be set based on your server configurations
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
