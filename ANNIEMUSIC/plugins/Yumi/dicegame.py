387578a8e308b9b720eb51701d664942536c95bf
Git-Hub-xD <166714901+Git-Hub-xD@users.noreply.github.com>
2024-05-12 19:22:40 +0530

    Update dicegame.py

--- ANNIEMUSIC/plugins/Yumi/dicegame.py
+++ ANNIEMUSIC/plugins/Yumi/dicegame.py
@@ -10,42 +10,42 @@ from pyrogram.handlers import MessageHandler
 async def dice(bot, message):
     x=await bot.send_dice(message.chat.id)
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
   
 @app.on_message(filters.command("dart"))
 async def dart(bot, message):
     x=await bot.send_dice(message.chat.id, "🎯")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
 
 @app.on_message(filters.command("basket"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🏀")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
 @app.on_message(filters.command("jackpot"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎰")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
 @app.on_message(filters.command("ball"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "🎳")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
 @app.on_message(filters.command("football"))
 async def basket(bot, message):
     x=await bot.send_dice(message.chat.id, "⚽")
     m=x.dice.value
-    await message.reply_text(f"Hey {message.from_user.mention} your Score is : {m}",quote=True)
+    await message.reply_text(f"ʜᴇʏ {message.from_user.mention} ʏᴏᴜʀ sᴄᴏʀᴇ ɪs : {m}",quote=True)
 __help__ = """
  Play Game With Emojis:
-/dice - Dice 🎲
-/dart - Dart 🎯
-/basket - Basket Ball 🏀
-/ball - Bowling Ball 🎳
-/football - Football ⚽
-/jackpot - Spin slot machine 🎰
+/dice - ᴅɪᴄᴇ 🎲
+/dart - ᴅᴀʀᴛ 🎯
+/basket - ʙᴀsᴋᴇᴛ ʙᴀʟʟ 🏀
+/ball - ʙᴏᴡʟɪɴɢ ʙᴀʟʟ 🎳
+/football - ғᴏᴏᴛʙᴀʟʟ ⚽
+/jackpot - sᴘɪɴ sʟᴏᴛ ᴍᴀᴄʜɪɴᴇ 🎰
  """
 
 __mod_name__ = "Dɪᴄᴇ"
